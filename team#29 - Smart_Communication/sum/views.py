2bc134fd3b9d169c863204d5f9331c2ad77bd225
Aman Raj <archanaamanraj@gmail.com>
2019-10-05 13:27:56 +0530

    Add team#29

---
+++ team#29 - Smart_Communication/sum/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def guide(request):
+    
+
+    
+    return render(request,"sum/guide.html")  
